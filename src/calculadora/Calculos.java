3fec9c4c12e5f6cabefa1cae235b966d1e6a4581
Geyner1320 <geiner.rivera@uao.edu.co>
2021-04-22 12:12:11 -0500

    Clase Calculos.java con algunos metodos en revision

---
+++ src/calculadora/Calculos.java
@@ -0,0 +1,77 @@
+
+package calculadora;
+
+public class Calculos {
+    double num1;
+    double num2;
+
+    public Calculos(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+    
+    // Metodo para la suma
+    public int sumar(double a, double b){
+        int res = 0;
+        
+        res =  (int)(a + b) ;
+        
+        return res;
+    }
+    
+    // Metodo para la potencia
+    public int potencia(double b, double p){
+        int res = 0;
+        
+        res = (int)Math.pow(b, p);
+        
+        return res;
+    }
+    
+    
+    // Metodo para la raiz cuadrada
+    public double raiz (double a){
+        double res = 0.0;
+        
+        res = Math.sqrt(a);
+        
+        return res;
+    }
+    
+    // Metodo para numero aleatorio
+    // donde a es el numero mayor y b el numero menor
+    public int aleatorio(double a,double b){
+        int res = 0;
+        
+        res =  (int)((Math.random())*(a-b+1)+b);
+        
+        return res;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
